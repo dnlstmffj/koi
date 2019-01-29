d2ccb942b076772d0d19552f05c1151b530310fc
LucomStudio (강창완) <dnlstmffj@gmail.com>
2019-01-29 19:21:05 +0900

    15975

--- BOJ_15975/BOJ_15975.cpp
+++ BOJ_15975/BOJ_15975.cpp
@@ -8,7 +8,7 @@ using namespace std;
 struct var {
     int x, p;
 };
-struct var xp[100001] = {0, };
+struct var xp[100001];
 
 bool comp (const var &a, const var &b) {
     if(a.p<b.p) return true;
@@ -23,26 +23,32 @@ int h[100000];
 int main() {
     int n, i, a, b, sum=0;
     scanf("%d", &n);
-    for(i=0; i<n; i++) {
+    for(i=1; i<=n; i++) {
         scanf("%d %d", &a, &b);
         xp[i].x = a;
         xp[i].p = b;
     }
+    xp[0].p = 1000000000;
+    xp[0].x = 1000000000;
     sort(xp, xp+n, comp);
-    for(i=0; i<n; i++) {
-        if(xp[i].p == xp[i+1].p) { 
+    for(i=1; i<n; i++) {
+        if((xp[i].p == xp[i+1].p) && ((xp[i+1].x - xp[i].x) <= (xp[i].x - xp[i-1].x))) { 
             sum+= xp[i+1].x - xp[i].x;
-            h[i] = 1;
-            printf("%d %d\n", xp[i+1].x - xp[i].x, i);
-            }
+           
+        } else if ((xp[i].p == xp[i-1].p) && ((xp[i+1].x - xp[i].x) > (xp[i].x - xp[i-1].x))) {
+            sum+= xp[i].x - xp[i-1].x;
+        } else if (xp[i].p == xp[i-1].p) {
+            sum+= xp[i].x - xp[i-1].x;
+        } else if (xp[i].p == xp[i+1].p) { 
+            sum+= xp[i+1].x - xp[i].x;
+        }
+        else {
+
+
+        }
         
     }
-    for(i=n-1; i>=0; i--) {
-        if(xp[i].p == xp[i-1].p && h[i] == 0) {
-            sum+= xp[i].x - xp[i-1].x;
-            printf("%d %d\n", xp[i].x - xp[i-1].x, i);
-        }
-    }
+ 
 
     printf("%d", sum);
 
